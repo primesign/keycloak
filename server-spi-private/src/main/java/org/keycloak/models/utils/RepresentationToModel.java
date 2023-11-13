135ec6408cab933aa064902bd6fb2130af7f1352
Otto Touzil <otto.touzil@prime-sign.com>
2023-11-13 10:24:47 +0100

    fix not-updating clientScopes

--- server-spi-private/src/main/java/org/keycloak/models/utils/RepresentationToModel.java
+++ server-spi-private/src/main/java/org/keycloak/models/utils/RepresentationToModel.java
@@ -460,28 +460,7 @@ public class RepresentationToModel {
             addClientScopeToClient(realm, client, clientTemplateName, true);
         }
 
-        if (resourceRep.getDefaultClientScopes() != null || resourceRep.getOptionalClientScopes() != null) {
-            // First remove all default/built in client scopes
-            for (ClientScopeModel clientScope : client.getClientScopes(true).values()) {
-                client.removeClientScope(clientScope);
-            }
-
-            // First remove all default/built in client scopes
-            for (ClientScopeModel clientScope : client.getClientScopes(false).values()) {
-                client.removeClientScope(clientScope);
-            }
-        }
-
-        if (resourceRep.getDefaultClientScopes() != null) {
-            for (String clientScopeName : resourceRep.getDefaultClientScopes()) {
-                addClientScopeToClient(realm, client, clientScopeName, true);
-            }
-        }
-        if (resourceRep.getOptionalClientScopes() != null) {
-            for (String clientScopeName : resourceRep.getOptionalClientScopes()) {
-                addClientScopeToClient(realm, client, clientScopeName, false);
-            }
-        }
+        updateClientScopes(resourceRep, client);
 
         if (resourceRep.isFullScopeAllowed() != null) {
             client.setFullScopeAllowed(resourceRep.isFullScopeAllowed());
@@ -583,6 +562,8 @@ public class RepresentationToModel {
             }
         }
 
+        updateClientScopes(rep, resource);
+
         if (resource.isPublicClient() || resource.isBearerOnly()) {
             resource.setSecret(null);
         } else {
@@ -623,6 +604,32 @@ public class RepresentationToModel {
             session.getKeycloakSessionFactory().publish(event);
         }
     }
+
+    private static void updateClientScopes(ClientRepresentation representation, ClientModel clientModelResource) {
+        if (representation.getDefaultClientScopes() != null || representation.getOptionalClientScopes() != null) {
+            // First remove all default/built in client scopes
+            for (ClientScopeModel clientScope :  clientModelResource.getClientScopes(true).values()) {
+                 clientModelResource.removeClientScope(clientScope);
+            }
+
+            // First remove all default/built in client scopes
+            for (ClientScopeModel clientScope :  clientModelResource.getClientScopes(false).values()) {
+                 clientModelResource.removeClientScope(clientScope);
+            }
+        }
+
+        if (representation.getDefaultClientScopes() != null) {
+            for (String clientScopeName : representation.getDefaultClientScopes()) {
+                addClientScopeToClient( clientModelResource.getRealm(),  clientModelResource, clientScopeName, true);
+            }
+        }
+        if (representation.getOptionalClientScopes() != null) {
+            for (String clientScopeName : representation.getOptionalClientScopes()) {
+                addClientScopeToClient( clientModelResource.getRealm(),  clientModelResource, clientScopeName, false);
+            }
+        }
+    }
+
 
     public static void updateClientProtocolMappers(ClientRepresentation rep, ClientModel resource) {
 
