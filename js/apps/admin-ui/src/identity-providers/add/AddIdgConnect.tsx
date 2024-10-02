ebd6d5f32968a9b98d51e14cb98180c76c6cc003
Otto Touzil <otto.touzil@prime-sign.com>
2024-10-02 11:05:03 +0200

    update admin ui for german-eid

--- js/apps/admin-ui/src/identity-providers/add/AddIdgConnect.tsx
+++ js/apps/admin-ui/src/identity-providers/add/AddIdgConnect.tsx
@@ -9,7 +9,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { Link, useNavigate } from "react-router-dom";
 
-import { adminClient } from "../../admin-client";
+import { useAdminClient } from "../../admin-client";
 import { useAlerts } from "../../components/alert/Alerts";
 import { FormAccess } from "../../components/form/FormAccess";
 import { ViewHeader } from "../../components/view-header/ViewHeader";
@@ -23,12 +23,17 @@ type DiscoveryIdentityProvider = IdentityProviderRepresentation & {
 };
 
 export default function AddIdgConnect() {
+  const {adminClient} = useAdminClient();
+
   const { t } = useTranslation();
   const navigate = useNavigate();
   const id = "german-eid";
 
   const form = useForm<DiscoveryIdentityProvider>({
-    defaultValues: { alias: id, config: { allowCreate: "true" } },
+    defaultValues: {
+      alias: id,
+      config: { allowCreate: "true" }},
+      mode: "onChange",
   });
   const {
     handleSubmit,
