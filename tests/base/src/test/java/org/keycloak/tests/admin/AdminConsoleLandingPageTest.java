74c5e8a13258c4d6d0fd2a8d5854ac68c71283f4
Otto Touzil <otto.touzil@prime-sign.com>
2025-05-16 09:01:49 +0200

    fix admin ui test

--- tests/base/src/test/java/org/keycloak/tests/admin/AdminConsoleLandingPageTest.java
+++ tests/base/src/test/java/org/keycloak/tests/admin/AdminConsoleLandingPageTest.java
@@ -34,7 +34,7 @@ public class AdminConsoleLandingPageTest {
         Assertions.assertEquals(keycloakUrls.getBaseUrl().toString()+ "", authUrl);
 
         String resourceUrl = config.get("resourceUrl");
-        Assertions.assertTrue(resourceUrl.matches("/resources/[^/]*/admin/keycloak.v2"));
+        Assertions.assertTrue(resourceUrl.matches("/resources/[^/]*/admin/primesign.v2"));
 
         String consoleBaseUrl = config.get("consoleBaseUrl");
         Assertions.assertEquals(consoleBaseUrl, "/admin/master/console/");
