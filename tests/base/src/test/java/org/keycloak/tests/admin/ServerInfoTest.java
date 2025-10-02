19cd6e8ae0e0855bb4aa07a1c07e6a6af1eac9c1
Patrick Weiner <patrick.weiner@prime-sign.com>
2025-10-02 11:42:18 +0200

    [Admin-Theme]: Rename admin-theme.

--- tests/base/src/test/java/org/keycloak/tests/admin/ServerInfoTest.java
+++ tests/base/src/test/java/org/keycloak/tests/admin/ServerInfoTest.java
@@ -47,60 +47,60 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @KeycloakIntegrationTest
 public class ServerInfoTest {
 
-    @InjectAdminClient
-    Keycloak adminClient;
+	@InjectAdminClient
+	Keycloak adminClient;
 
-    @InjectCryptoHelper
-    CryptoHelper cryptoHelper;
+	@InjectCryptoHelper
+	CryptoHelper cryptoHelper;
 
-    @Test
-    public void testServerInfo() {
-        ServerInfoRepresentation info = adminClient.serverInfo().getInfo();
-        assertNotNull(info);
+	@Test
+	public void testServerInfo() {
+		ServerInfoRepresentation info = adminClient.serverInfo().getInfo();
+		assertNotNull(info);
 
-        assertNotNull(info.getProviders());
-        assertNotNull(info.getProviders().get("realm"));
-        assertNotNull(info.getProviders().get("user"));
-        assertNotNull(info.getProviders().get("authenticator"));
+		assertNotNull(info.getProviders());
+		assertNotNull(info.getProviders().get("realm"));
+		assertNotNull(info.getProviders().get("user"));
+		assertNotNull(info.getProviders().get("authenticator"));
 
-        assertNotNull(info.getThemes());
-        assertNotNull(info.getThemes().get("account"));
-        Assert.assertNames(info.getThemes().get("account"), "base", "keycloak.v3");
-        Assert.assertNames(info.getThemes().get("admin"), "base", "keycloak.v2");
-        Assert.assertNames(info.getThemes().get("email"), "base", "keycloak");
-        Assert.assertNames(info.getThemes().get("login"), "base", "keycloak", "keycloak.v2");
-        Assert.assertNames(info.getThemes().get("welcome"), "keycloak");
+		assertNotNull(info.getThemes());
+		assertNotNull(info.getThemes().get("account"));
+		Assert.assertNames(info.getThemes().get("account"), "base", "keycloak.v3");
+		Assert.assertNames(info.getThemes().get("admin"), "base", "primesign.v2");
+		Assert.assertNames(info.getThemes().get("email"), "base", "keycloak");
+		Assert.assertNames(info.getThemes().get("login"), "base", "keycloak", "keycloak.v2");
+		Assert.assertNames(info.getThemes().get("welcome"), "keycloak");
 
-        assertNotNull(info.getEnums());
+		assertNotNull(info.getEnums());
 
-        assertNotNull(info.getMemoryInfo());
-        assertNotNull(info.getSystemInfo());
+		assertNotNull(info.getMemoryInfo());
+		assertNotNull(info.getSystemInfo());
 
-        assertNotNull(info.getCryptoInfo());
-        Assert.assertNames(info.getCryptoInfo().getSupportedKeystoreTypes(), cryptoHelper.getExpectedSupportedKeyStoreTypes());
-        Assert.assertNames(info.getCryptoInfo().getClientSignatureSymmetricAlgorithms(), Algorithm.HS256, Algorithm.HS384, Algorithm.HS512);
-        Assert.assertNames(info.getCryptoInfo().getClientSignatureAsymmetricAlgorithms(),
-                Algorithm.ES256, Algorithm.ES384, Algorithm.ES512,
-                Algorithm.EdDSA, Algorithm.PS256, Algorithm.PS384,
-                Algorithm.PS512, Algorithm.RS256, Algorithm.RS384,
-                Algorithm.RS512);
+		assertNotNull(info.getCryptoInfo());
+		Assert.assertNames(info.getCryptoInfo().getSupportedKeystoreTypes(), cryptoHelper.getExpectedSupportedKeyStoreTypes());
+		Assert.assertNames(info.getCryptoInfo().getClientSignatureSymmetricAlgorithms(), Algorithm.HS256, Algorithm.HS384, Algorithm.HS512);
+		Assert.assertNames(info.getCryptoInfo().getClientSignatureAsymmetricAlgorithms(),
+			Algorithm.ES256, Algorithm.ES384, Algorithm.ES512,
+			Algorithm.EdDSA, Algorithm.PS256, Algorithm.PS384,
+			Algorithm.PS512, Algorithm.RS256, Algorithm.RS384,
+			Algorithm.RS512);
 
-        ComponentTypeRepresentation rsaGeneratedProviderInfo = info.getComponentTypes().get(KeyProvider.class.getName())
-                .stream()
-                .filter(componentType -> GeneratedRsaKeyProviderFactory.ID.equals(componentType.getId()))
-                .findFirst().orElseThrow(() -> new RuntimeException("Not found provider with ID 'rsa-generated'"));
-        ConfigPropertyRepresentation keySizeRep = rsaGeneratedProviderInfo.getProperties()
-                .stream()
-                .filter(configProp -> Attributes.KEY_SIZE_KEY.equals(configProp.getName()))
-                .findFirst().orElseThrow(() -> new RuntimeException("Not found provider with ID 'rsa-generated'"));
-        Assert.assertNames(keySizeRep.getOptions(), cryptoHelper.getExpectedSupportedRsaKeySizes());
+		ComponentTypeRepresentation rsaGeneratedProviderInfo = info.getComponentTypes().get(KeyProvider.class.getName())
+			.stream()
+			.filter(componentType -> GeneratedRsaKeyProviderFactory.ID.equals(componentType.getId()))
+			.findFirst().orElseThrow(() -> new RuntimeException("Not found provider with ID 'rsa-generated'"));
+		ConfigPropertyRepresentation keySizeRep = rsaGeneratedProviderInfo.getProperties()
+			.stream()
+			.filter(configProp -> Attributes.KEY_SIZE_KEY.equals(configProp.getName()))
+			.findFirst().orElseThrow(() -> new RuntimeException("Not found provider with ID 'rsa-generated'"));
+		Assert.assertNames(keySizeRep.getOptions(), cryptoHelper.getExpectedSupportedRsaKeySizes());
 
-        assertEquals(Version.VERSION, info.getSystemInfo().getVersion());
-        assertNotNull(info.getSystemInfo().getServerTime());
-        assertNotNull(info.getSystemInfo().getUptime());
+		assertEquals(Version.VERSION, info.getSystemInfo().getVersion());
+		assertNotNull(info.getSystemInfo().getServerTime());
+		assertNotNull(info.getSystemInfo().getUptime());
 
-        Map<String, ProviderRepresentation> jpaProviders = info.getProviders().get("connectionsJpa").getProviders();
-        ProviderRepresentation jpaProvider = jpaProviders.values().iterator().next();
-        Assertions.assertNotNull(jpaProvider.getOperationalInfo());
-    }
+		Map<String, ProviderRepresentation> jpaProviders = info.getProviders().get("connectionsJpa").getProviders();
+		ProviderRepresentation jpaProvider = jpaProviders.values().iterator().next();
+		Assertions.assertNotNull(jpaProvider.getOperationalInfo());
+	}
 }
